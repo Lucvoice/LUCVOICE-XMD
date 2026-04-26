a9f63561a8be1cf77bdc5c21f700f6ced545a414
Lucvoice <simkondabrand@gmail.com>
2026-04-26 19:47:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -678,12 +678,12 @@ setTimeout(() => {
                 if (conf.DP?.toLowerCase() === 'yes') {
                     let mode = conf.MODE?.toLowerCase() === 'yes' ? 'public' : 'private';
                     let msg = `
-╭━━━〔 ${BOT_NAME} 〕━━━╮
-┃ 🤖 STATUS   : CONNECTED ✅
-┃ 🌐 MODE     : ${mode.toUpperCase()} 🟢
-┃ 💫 PREFIX   : [ ${prefixe} ]
-┃ ⚡ SPEED    : FAST ⚡
-╰━━━━━━━━━━━━━━━━━━━━━━╯
+╭─────────────━┈⊷
+│ 🌏 ${BOT_NAME} 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃
+│ 💫 Prefix: [ ${prefixe} ]
+│ ⭕ Mode: ${mode}
+│ ⚡ Speed: FAST ⚡
+╰─────────────━┈⊷
 
 ${FOOTER_TEXT}
 `;
